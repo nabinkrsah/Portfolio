355be945d120738f5b68e7e64b20936e864e14b3
nabinkrsah <nabinsah62246@gmail.com>
2025-01-31 09:42:05 +0530

    add files

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const typed = new Typed(".text", {
+    strings: [
+      "Web Developer",
+      "Software Designer",
+      "Mobile App Developer",
+      "Data Science Enthusiast",
+    ], // Text to type
+    typeSpeed: 50, // Typing speed in milliseconds
+    backSpeed: 25, // Backspacing speed
+    loop: true, // Repeat the typing animation
+  });
+});
